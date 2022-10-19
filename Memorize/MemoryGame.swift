7c71950a9be0aa9cc70f84fce617c00051d2a399
Brio <info@briomusic.net>
2022-10-19 09:38:00 +0200

    simplify model

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,28 +10,29 @@ import Foundation
 struct MemoryGame<CardContent: Equatable> {
     private(set) var cards: Array<Card>
     
-    private var indexOfFaceUpCard: Int?
+    private var indexOfFaceUpCard: Int? {
+        
+        get { cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly }
+        
+        set { cards.indices.forEach {cards[$0].isFaceUp = ($0 == newValue)} }
+    }
     
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
            !cards[chosenIndex].isFaceUp,
            !cards[chosenIndex].isMatched {
+            
             if let potentialIndex = indexOfFaceUpCard {
+                
                 if cards[chosenIndex].content == cards[potentialIndex].content {
                     cards[chosenIndex].isMatched = true
                     cards[potentialIndex].isMatched = true
                 }
-                indexOfFaceUpCard = nil
+                cards[chosenIndex].isFaceUp = true
+                
             } else {
-                for index in cards.indices {
-                    cards[index].isFaceUp = false
-                }
                 indexOfFaceUpCard = chosenIndex
             }
-            
-            cards[chosenIndex].isFaceUp.toggle()
-            print("chosen card:\(cards[chosenIndex])")
-            print("\(cards)")
         }
     }
     
@@ -62,5 +63,15 @@ struct MemoryGame<CardContent: Equatable> {
         var isMatched = false
         var content: CardContent
         var id: Int
+    }
+}
+
+
+extension Array {
+    var oneAndOnly: Element? {
+        if count == 1 {
+            return first
+        }
+        return nil
     }
 }
